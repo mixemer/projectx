01b30dd7ae15939950aba2ab2d915288767e0829
Mehmet Sahin <mehmetshin96@gmail.com>
2022-05-07 12:39:47 -0400

    Add TrashIsland (needs optimization, needs UI)

---
+++ Assets/Scripts/TrashIsland.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashIsland : MonoBehaviour
+{
+    public GameSceneManager sceneManager;
+    public GameObject part1;
+    public GameObject part2;
+    public GameObject part3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        part1.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sceneManager.GetGameStage() == GameSceneManager.GameStage.Mid)
+        {
+            part2.SetActive(true);
+        }
+
+        if (sceneManager.GetGameStage() == GameSceneManager.GameStage.Ending)
+        {
+            part3.SetActive(true);
+        }
+    }
+}
